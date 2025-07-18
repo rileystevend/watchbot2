7583d939303ee5f9f38e6524c652b411c0ff1508
Steven Riley <33920893+rileystevend@users.noreply.github.com>
2025-07-17 22:55:01 -0500

    Update scraper.py

--- helpers/scraper.py
+++ helpers/scraper.py
@@ -9,6 +9,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 logger = logging.getLogger('scraper')
 
